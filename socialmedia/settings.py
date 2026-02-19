d16c8f393e8288f8c59a0388dcbc1fa6cc8e25cd
casmircodes <72873224+casmircodes@users.noreply.github.com>
2026-02-19 12:39:10 +0100

    Fix MEDIA_URL path to start with a slash

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -125,5 +125,5 @@ STATIC_ROOT = BASE_DIR/'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [BASE_DIR/'static']
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
